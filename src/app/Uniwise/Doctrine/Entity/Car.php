44b320cdc70d498d3c05efc8457ba191f0b4b06f
12tare21 <tarik-muminovic@outlook.com>
2022-07-03 11:23:58 +0200

    Define Car entity

--- src/app/Uniwise/Doctrine/Entity/Car.php
+++ src/app/Uniwise/Doctrine/Entity/Car.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Uniwise\Doctrine\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -7,18 +9,37 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity(repositoryClass="CarRepository")
  * @ORM\Table(name="car")
  */
-class Car {
+class Car
+{
+    use Identifiable;
 
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=40)
+     * @ORM\Column(name="brand", type="string", length=255, nullable=false)
      * @var string
      */
-    private $make;
+    private $brand;
+
+    /**
+     * @ORM\Column(name="model", type="string", length=255, nullable=false)
+     * @var string
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(name="color", type="string", length=100, nullable=false)
+     * @var string
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(name="gas_economy", type="float", nullable=false)
+     * @var float
+     */
+    private $gasEconomy; // in MPG
+
+    /**
+     * @ORM\Column(name="extra_accessories", type="json", columnDefinition="json default '{}'")
+     * @var array
+     */
+    private $extraAccessories;
 }
