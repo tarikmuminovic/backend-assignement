cd9196c26ccff66d0e90874b977c3fc8e52a97ae
12tare21 <tarik-muminovic@outlook.com>
2022-07-03 11:36:50 +0200

    Redefine get cars endpoint & define its implementation

--- src/app/Uniwise/Symfony/Bundle/ApiBundle/Controller/Car/CarController.php
+++ src/app/Uniwise/Symfony/Bundle/ApiBundle/Controller/Car/CarController.php
@@ -1,20 +1,42 @@
 <?php
+declare(strict_types=1);
 
 namespace Uniwise\Symfony\Bundle\ApiBundle\Controller\Car;
 
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Route;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Uniwise\Symfony\Bundle\ApiBundle\Request\GetCarsRequest;
+use Uniwise\Symfony\Service\CarService;
 
 /**
- * @Route("/car")
+ * @Route("/cars")
  */
-class CarController extends FOSRestController {
+class CarController extends FOSRestController
+{
 
     /**
-     * @Get("")
+     * @var CarService
      */
-    public function getCars() {
-        return $this->view("Not implemented yet");
+    private $carService;
+
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
+
+    /**
+     * @param Request $request
+     * @Get("")
+     *
+     * @return View
+     */
+    public function getCars(Request $request): View
+    {
+        $getCarsRequest = new GetCarsRequest($request->query->all());
+        $cars = $this->carService->getAll($getCarsRequest);
+        return $this->view($cars);
     }
 }
